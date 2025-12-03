7e9aef5d732a13c21c99a0626919777a8d091ee5
Kub <95572194+Kub228@users.noreply.github.com>
2025-12-03 18:28:50 +0100

    day2 pt1

---
+++ 2025/d2.py
@@ -0,0 +1,27 @@
+inpt = '''11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124'''
+
+with open("txt_files\\d2.txt") as f:
+  inpt = f.read()
+
+inpt = inpt.replace("\n", "")
+inpt = inpt.strip()
+inpt = inpt.split(",")
+
+invalid_ids = []
+
+for i in range(len(inpt)):
+    inpt[i] = inpt[i].split("-")
+
+for i in range(len(inpt)):
+    for j in range(int(inpt[i][0]),int(inpt[i][1])+1):
+        mid=len(str(j))//2
+        if str(j)[:mid] == str(j)[mid:]:
+            print(f'{i} is invalid')
+            invalid_ids.append(j)
+
+print(invalid_ids)
+print(sum(invalid_ids))
+
+    
---
+++ 2025/t.py
@@ -0,0 +1,9 @@
+invalid_id = ''
+for i in range(95,115+1):
+    temp = ''
+    j = 0
+    while j < len(str(i)):
+        if temp != str(i):
+
+        
+            
